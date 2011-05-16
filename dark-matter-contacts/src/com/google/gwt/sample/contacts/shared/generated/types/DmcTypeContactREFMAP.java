c6214b6cd34be15ae0fd92e9cf4ff928a3b31214
pstrong99 <pstrong99@gmail.com>
2011-05-16 20:52:51 +0000

    Work on the example

--- dark-matter-contacts/src/com/google/gwt/sample/contacts/shared/generated/types/DmcTypeContactREFMAP.java
+++ dark-matter-contacts/src/com/google/gwt/sample/contacts/shared/generated/types/DmcTypeContactREFMAP.java
@@ -15,8 +15,8 @@ import org.dmd.dmc.types.UUIDName;    // key type import
  * The DmcTypeContactREFMAP provides storage for a map of ContactREF
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from: org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2239)
- *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:493)
+ * Generated from:  org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2239)
+ *    Called from:  org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:493)
  */
 @SuppressWarnings("serial")
 // public class DmcTypeContactREFMAP extends DmcTypeContactREF<ContactREF,UUIDName> {
--- dark-matter-contacts/src/com/google/gwt/sample/contacts/shared/generated/types/DmcTypeContactREFMV.java
+++ dark-matter-contacts/src/com/google/gwt/sample/contacts/shared/generated/types/DmcTypeContactREFMV.java
@@ -10,8 +10,8 @@ import org.dmd.dmc.DmcValueException;
  * The DmcTypeContactREFMV provides storage for a multi-valued ContactREF
  * <P>
  * This code was auto-generated and shouldn't be altered manually!
- * Generated from: org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1841)
- *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:484)
+ * Generated from:  org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:1841)
+ *    Called from:  org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:484)
  */
 @SuppressWarnings("serial")
 public class DmcTypeContactREFMV extends DmcTypeContactREF implements Serializable {
