e6139841e322a44729fb55f04c2395ea56489a25
pstrong99@gmail.com <pstrong99@gmail.com@5f1ac065-0305-f0e0-43e8-61afe54a7d34>
2011-05-24 03:05:04 +0000

    MVW work

--- trunk/dark-matter-contacts/src/com/google/gwt/sample/contacts/client/generated/mvw/events/EditContactCancelledEventHandler.java
+++ trunk/dark-matter-contacts/src/com/google/gwt/sample/contacts/client/generated/mvw/events/EditContactCancelledEventHandler.java
@@ -1,7 +1,7 @@
 package com.google.gwt.sample.contacts.client.generated.mvw.events;
 
-// Generated from:  org.dmd.util.codegen.ImportManager.getFormattedImports(ImportManager.java:34)
-// Called from:  org.dmd.mvw.tools.mvwgenerator.util.GwtEventFormatter.dumpHandler(GwtEventFormatter.java:158)
+// Generated from: org.dmd.util.codegen.ImportManager.getFormattedImportsStatic(ImportManager.java:37)
+// Called from: org.dmd.mvw.tools.mvwgenerator.util.GwtEventFormatter.dumpHandler(GwtEventFormatter.java:163)
 import com.google.gwt.event.shared.EventHandler;         // The marker interface
 
 public interface EditContactCancelledEventHandler extends EventHandler {
