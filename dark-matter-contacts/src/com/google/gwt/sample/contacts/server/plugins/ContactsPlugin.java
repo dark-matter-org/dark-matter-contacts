f5c8d5e53b42b8b7cdc6f8f8ea6dc3a3dd7c3014
pstrong99 <pstrong99@gmail.com>
2012-04-17 20:28:02 +0000

    Continuing development of the contacts example.

--- dark-matter-contacts/src/com/google/gwt/sample/contacts/server/plugins/ContactsPlugin.java
+++ dark-matter-contacts/src/com/google/gwt/sample/contacts/server/plugins/ContactsPlugin.java
@@ -1,24 +1,42 @@
 package com.google.gwt.sample.contacts.server.plugins;
 
+import org.dmd.dmc.DmcValueException;
 import org.dmd.dmp.server.servlet.base.DmpServletPlugin;
 import org.dmd.util.exceptions.DebugInfo;
+import org.dmd.util.exceptions.ResultException;
 
+import com.google.gwt.sample.contacts.server.generated.ContactsSchemaAG;
 import com.google.gwt.sample.contacts.shared.generated.dmo.ContactsDMSAG;
 
+/**
+ * The ContactsPlugin provides an example of extending the basic capabilities of
+ * the DMP Servlet Reference Implementation. 
+ */
 public class ContactsPlugin extends DmpServletPlugin {
 	
 	public ContactsPlugin(){
-		
 	}
 	
 	@Override
-	public void preInit(){
+	public void preInit() throws DmcValueException, ResultException{
 		DebugInfo.debug("ContactsPlugin.preInit()");
+		
+		// Add our schema
+		ContactsSchemaAG contacts = new ContactsSchemaAG();
+		pluginManager.getSchema().manageSchema(contacts);
 		
 		// Indicate that we need to index our Contact entries
 		cache.maintainIndex(ContactsDMSAG.__Contact);
 	}
 	
-	
+	@Override
+	public void init(){
+		if (cache.getIndexSize(ContactsDMSAG.__Contact) == 0){
+			DebugInfo.debug("We have no Contact instances, we'll create some...");
+		}
+		else{
+			DebugInfo.debug("We have " + cache.getIndexSize(ContactsDMSAG.__Contact) + " Contacts");
+		}
+	}
 
 }
