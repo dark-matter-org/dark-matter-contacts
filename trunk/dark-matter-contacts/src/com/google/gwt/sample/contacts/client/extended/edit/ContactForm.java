8fc6b9cbdafb6ca54f0c012e772c3519fdb12f33
pstrong99@gmail.com <pstrong99@gmail.com@5f1ac065-0305-f0e0-43e8-61afe54a7d34>
2012-04-27 21:11:56 +0000

    Continuing development of the contacts example.

---
+++ trunk/dark-matter-contacts/src/com/google/gwt/sample/contacts/client/extended/edit/ContactForm.java
@@ -0,0 +1,10 @@
+package com.google.gwt.sample.contacts.client.extended.edit;
+
+import org.dmd.dmc.presentation.Tracker;
+import org.dmd.mvw.client.gxtforms.GxtFormPanel;
+
+public class ContactForm extends GxtFormPanel {
+	
+	Tracker	tracker;
+
+}
