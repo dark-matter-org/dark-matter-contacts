38b95391e2946b54d36d122422c2ee861ce966db
pstrong99@gmail.com <pstrong99@gmail.com@5f1ac065-0305-f0e0-43e8-61afe54a7d34>
2012-06-11 16:52:14 +0000

    Changes to make use of the new enum mapping generator mechanisms in MVW/GXT.

--- branches/1.0/src/com/google/gwt/sample/contacts/client/mvwmodule/contacts/generated/mvw/forms/ContactTypeEnumMapping.java
+++ branches/1.0/src/com/google/gwt/sample/contacts/client/mvwmodule/contacts/generated/mvw/forms/ContactTypeEnumMapping.java
@@ -1,13 +1,13 @@
 package com.google.gwt.sample.contacts.client.mvwmodule.contacts.generated.mvw.forms;
 
 // Generated from: org.dmd.util.codegen.ImportManager.getFormattedImports(ImportManager.java:82)
-// Called from: org.dmd.mvw.tools.mvwgenerator.util.GxtEnumMappingFormatter.formatEnumMapping(GxtEnumMappingFormatter.java:28)
-import java.util.ArrayList;                                                 // Our mapping type - (GxtEnumMappingFormatter.java:21)
-import java.util.List;                                                      // Required by combo box - (GxtEnumMappingFormatter.java:22)
-import org.dmd.mvw.client.gxtforms.implementation.GxtEnumMappingIF;         // Our interface - (GxtEnumMappingFormatter.java:24)
-import org.dmd.mvw.client.gxtforms.implementation.GxtEnumValue;             // Our base enum representation - (GxtEnumMappingFormatter.java:23)
+// Called from: org.dmd.mvw.extensions.GxtEnumMappingFormatter.formatEnumMapping(GxtEnumMappingFormatter.java:33)
+import java.util.ArrayList;                                                 // Our mapping type - (GxtEnumMappingFormatter.java:26)
+import java.util.List;                                                      // Required by combo box - (GxtEnumMappingFormatter.java:27)
+import org.dmd.mvw.client.gxtforms.implementation.GxtEnumMappingIF;         // Our interface - (GxtEnumMappingFormatter.java:29)
+import org.dmd.mvw.client.gxtforms.implementation.GxtEnumValue;             // Our base enum representation - (GxtEnumMappingFormatter.java:28)
 
-// org.dmd.mvw.tools.mvwgenerator.util.GxtEnumMappingFormatter.formatEnumMapping(GxtEnumMappingFormatter.java:30)
+// org.dmd.mvw.extensions.GxtEnumMappingFormatter.formatEnumMapping(GxtEnumMappingFormatter.java:35)
 public class ContactTypeEnumMapping implements GxtEnumMappingIF {
 
     static ContactTypeEnumMapping instance;
